727503f993788bb0fa77fd82c37f279fefed4f65
Brian Haller <brianshaller@gmail.com>
2019-08-28 18:06:14 -0600

    commit

--- dashboard/src/components/good_widgets/AddWidget.js
+++ dashboard/src/components/good_widgets/AddWidget.js
@@ -21,7 +21,7 @@ class AddWidget extends Component {
           <p className="add-widget-error">{this.state.error}</p>
         )}
         <form className="add-widget" onSubmit={this.handleAdd.bind(this)}>
-          <input className="add-widget__input" type="text" name="newWidget" />
+          <input className="add-widget__input" type="text" name="newWidget" placeholder="5753946,5763297"/>
           <button className="button">Add widgets</button>
         </form>
       </div>
