d0e8f3cd05a631a23a2ea9c57dcc048ac81ad0c1
Brian Haller <brianshaller@gmail.com>
2019-08-31 18:46:06 -0600

    commit

--- update_traffic_click_id_in_conversions_table.py
+++ update_traffic_click_id_in_conversions_table.py
@@ -51,15 +51,27 @@ def update_traffic_click_id_in_conversions_table(days_ago):
     # mycursor.execute(sql)
     ####################
 
-    url = f"https://api.voluum.com/report/conversions?from={start_date}T00%3A00%3A00Z&to={end_date}T00%3A00%3A00Z&tz=America%2FLos_Angeles&sort=postbackTimestamp&direction=desc&columns=postbackTimestamp&columns=externalId&columns=clickId&columns=transactionId&columns=revenue&groupBy=conversion&offset=0&limit=100000&include=ACTIVE"
-    res = requests.get(url, headers = {"cwauth-token": token})
-    res = res.json()
-    for row in res["rows"]:
-        vol_click_id = row["clickId"]
-        mgid_click_id = row["externalId"]
-        sql = f"UPDATE conversions SET traffic_click_id='{mgid_click_id}' WHERE voluum_click_id='{vol_click_id}'"
-        mycursor.execute(sql)
-        mydb.commit()
+    url = f"https://api.voluum.com/report/conversions?from={start_date}T00%3A00%3A00Z&to={end_date}T00%3A00%3A00Z&tz=America%2FLos_Angeles&sort=postbackTimestamp&direction=desc&columns=postbackTimestamp&columns=externalId&columns=clickId&columns=transactionId&columns=revenue&groupBy=conversion&offset=0&limit=100000&include=ACTIVE" 
+    res = requests.get(url, headers = {"cwauth-token": token}) 
+    res = res.json() 
+    for row in res["rows"]: 
+        vol_click_id = row["clickId"] 
+        mgid_click_id = row["externalId"] 
+        conversion_type = row["transactionId"] 
+        postback_timestamp = row["postbackTimestamp"] 
+        postback_timestamp = datetime.strptime(postback_timestamp, '%Y-%m-%d %I:%M:%S %p') 
+        revenue = row["revenue"] 
+        sql = f"UPDATE conversions SET traffic_click_id='{mgid_click_id}' WHERE traffic_click_id=NULL AND voluum_click_id='{vol_click_id}'" 
+        mycursor.execute(sql) 
+        if mycursor.rowcount == 0: 
+            sql = f"SELECT COUNT(*) FROM conversions WHERE voluum_click_id='{vol_click_id}' AND conversion_type='{conversion_type}'" 
+            mycursor.execute(sql) 
+            for count in mycursor: 
+                if count[0] == 0: 
+                    sql = f"INSERT INTO conversions (`id`, `conversion_date`, `traffic_click_id`, `voluum_click_id`,`conversion_type`,`conversion_revenue`) VALUES (NULL, '{postback_timestamp}', '{mgid_click_id}', '{vol_click_id}', '{conversion_type}', '{revenue}')" 
+                    mycursor.execute(sql) 
+    
+        mydb.commit() 
 
 
 
