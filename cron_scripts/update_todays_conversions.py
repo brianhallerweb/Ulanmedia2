0590b6a01b97c76ab6d759b353a5122ec106f87c
Brian Haller <brianshaller@gmail.com>
2019-08-31 19:17:30 -0600

    commit

--- cron_scripts/update_todays_conversions.py
+++ cron_scripts/update_todays_conversions.py
@@ -1,3 +1,4 @@
 from update_traffic_click_id_in_conversions_table import update_traffic_click_id_in_conversions_table
 
 update_traffic_click_id_in_conversions_table(0)
+print("conversions table updated with mgid click ids")
