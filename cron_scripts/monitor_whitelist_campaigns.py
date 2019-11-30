fc75ff8d4782a38f7ed2451ed399d249c714bb3f
Brian Haller <brianshaller@gmail.com>
2019-11-30 08:33:14 -0700

    commit

--- cron_scripts/monitor_whitelist_campaigns.py
+++ cron_scripts/monitor_whitelist_campaigns.py
@@ -13,92 +13,100 @@ import pprint
 pp=pprint.PrettyPrinter(indent=2)
 
 def monitor_whitelist_campaigns():
-    # ##############
-    # get mgid token
-    
-    res = requests.post("https://api.mgid.com/v1/auth/token",
-            headers={"Content-type": "application/x-www-form-urlencoded",
-                     "Cache-Control": "no-cache"},
-             data={"email": mgid_login, "password": mgid_password})
-    
-    token = res.json()["token"] 
+    try:
+        # ##############
+        # get mgid token
+        
+        res = requests.post("https://api.mgid.com/v1/auth/token",
+                headers={"Content-type": "application/x-www-form-urlencoded",
+                         "Cache-Control": "no-cache"},
+                 data={"email": mgid_login, "password": mgid_password})
+        
+        token = res.json()["token"] 
 
-    #############
-    # mgid dates
-    
-    timezone = 'america/los_angeles'
-    start_date_utc = pytz.utc.localize(datetime.utcnow()) - timedelta(0)
-    start_date_pst = start_date_utc.astimezone(pytz.timezone(timezone))
-    end_date_utc = pytz.utc.localize(datetime.utcnow()) - timedelta(0)
-    end_date_pst = end_date_utc.astimezone(pytz.timezone(timezone))
-    
-    start_date = start_date_pst.strftime("%Y-%m-%d")
-    end_date = end_date_pst.strftime("%Y-%m-%d")
-
-
-    ##################
-    # set up mysql
-
-    mydb = mysql.connector.connect(
-      host="localhost",
-      user= mysql_user,
-      passwd= mysql_password,
-      database="ulanmedia"
-    )
-
-    mycursor = mydb.cursor()
-
-    sql = f"SELECT DISTINCT traffic_campaign_id from whitelist_campaigns"
-
-    mycursor.execute(sql)
-    result = mycursor.fetchall()
-    campaign_ids = []
-    for row in result:
-        campaign_ids.append(row[0])
+        #############
+        # mgid dates
+        
+        timezone = 'america/los_angeles'
+        start_date_utc = pytz.utc.localize(datetime.utcnow()) - timedelta(0)
+        start_date_pst = start_date_utc.astimezone(pytz.timezone(timezone))
+        end_date_utc = pytz.utc.localize(datetime.utcnow()) - timedelta(0)
+        end_date_pst = end_date_utc.astimezone(pytz.timezone(timezone))
+        
+        start_date = start_date_pst.strftime("%Y-%m-%d")
+        end_date = end_date_pst.strftime("%Y-%m-%d")
 
 
+        ##################
+        # set up mysql
 
-    for campaign_id in campaign_ids:
-        url=f"https://api.mgid.com/v1/goodhits/clients/{mgid_client_id}/campaigns/{campaign_id}?token={token}";
-        res = requests.get(url)
-        res = res.json()
-        excluded_widgets = []
-        if len(res["widgetsFilterUid"]["widgets"]) > 0:
-            for key, value in res["widgetsFilterUid"]["widgets"].items():
-                if (value == "[]") | (value == None):
-                    excluded_widgets.append(key)
-                else:
-                    value = value.replace("[", "")
-                    value = value.replace("]", "")
-                    value = value.replace(",", "")
-                    c_widgets = value.split(" ")
-                    for c_widget in c_widgets:
-                        excluded_widgets.append(f"{key}s{c_widget}")
+        mydb = mysql.connector.connect(
+          host="localhost",
+          user= mysql_user,
+          passwd= mysql_password,
+          database="ulanmedia"
+        )
 
-        sql = f"SELECT widget_id from whitelist_campaigns WHERE traffic_campaign_id = '{campaign_id}'"
+        mycursor = mydb.cursor()
+
+        sql = f"SELECT DISTINCT traffic_campaign_id from whitelist_campaigns"
+
         mycursor.execute(sql)
         result = mycursor.fetchall()
-        whitelist_widget_ids = []
+        campaign_ids = []
         for row in result:
-            whitelist_widget_ids.append(row[0])
+            campaign_ids.append(row[0])
 
-        url = f"https://api.mgid.com/v1/goodhits/campaigns/{campaign_id}/quality-analysis/?token={token}&dateInterval=today"
 
-        res = requests.get(url)
-        res = res.json()
-        for widget_id in res[campaign_id][f"{start_date}_{end_date}"]:
 
-            clicks = res[campaign_id][f"{start_date}_{end_date}"][widget_id]['clicks']
+        for campaign_id in campaign_ids:
+            url=f"https://api.mgid.com/v1/goodhits/clients/{mgid_client_id}/campaigns/{campaign_id}?token={token}";
+            res = requests.get(url)
+            res = res.json()
+            excluded_widgets = []
+            if len(res["widgetsFilterUid"]["widgets"]) > 0:
+                for key, value in res["widgetsFilterUid"]["widgets"].items():
+                    if (value == "[]") | (value == None):
+                        excluded_widgets.append(key)
+                    else:
+                        value = value.replace("[", "")
+                        value = value.replace("]", "")
+                        value = value.replace(",", "")
+                        c_widgets = value.split(" ")
+                        for c_widget in c_widgets:
+                            excluded_widgets.append(f"{key}s{c_widget}")
 
-            if widget_id in whitelist_widget_ids:
-                print(f"widget {widget_id} in whitelist")
-            elif widget_id not in whitelist_widget_ids and widget_id in excluded_widgets:
-                print(f"widget {widget_id} not in whitelist but is already excluded")
-            else:
-                print(f"widget {widget_id} needs to be paused")
-                url = f"https://api.mgid.com/v1/goodhits/clients/{mgid_client_id}/campaigns/{campaign_id}?token={token}";
-                response = requests.patch(url, headers ={"Content-Type": "application/x-www-form-urlencoded", "Cache-Control": "no-cache"}, data = {"widgetsFilterUid": f"include,except,{widget_id}"})
-                print(response.json())
+            sql = f"SELECT widget_id from whitelist_campaigns WHERE traffic_campaign_id = '{campaign_id}'"
+            mycursor.execute(sql)
+            result = mycursor.fetchall()
+            whitelist_widget_ids = []
+            for row in result:
+                whitelist_widget_ids.append(row[0])
+
+            url = f"https://api.mgid.com/v1/goodhits/campaigns/{campaign_id}/quality-analysis/?token={token}&dateInterval=today"
+
+            res = requests.get(url)
+            res = res.json()
+            for widget_id in res[campaign_id][f"{start_date}_{end_date}"]:
+
+                clicks = res[campaign_id][f"{start_date}_{end_date}"][widget_id]['clicks']
+
+                if widget_id in whitelist_widget_ids:
+                    #print(f"widget {widget_id} in whitelist")
+                    pass
+                elif widget_id not in whitelist_widget_ids and widget_id in excluded_widgets:
+                    #print(f"widget {widget_id} not in whitelist but is already excluded")
+                    pass
+                else:
+                    #print(f"widget {widget_id} needs to be paused")
+                    url = f"https://api.mgid.com/v1/goodhits/clients/{mgid_client_id}/campaigns/{campaign_id}?token={token}";
+                    response = requests.patch(url, headers ={"Content-Type": "application/x-www-form-urlencoded", "Cache-Control": "no-cache"}, data = {"widgetsFilterUid": f"include,except,{widget_id}"})
+                    #print(response.json())
+    except Exception as e:
+        print("Failed - email sent")
+        send_email("brianshaller@gmail.com", "Failed - create_campaigns_for_all_campaigns_dataset()", e)
+        sys.exit()
+
 
 
 monitor_whitelist_campaigns()
